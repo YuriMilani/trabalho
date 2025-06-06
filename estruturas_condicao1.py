973ea6327966c165819ed5aef4738d23bf3791b8
YuriMilani <{seu_email}>
2025-06-06 20:33:19 -0300

    primeiro commit

---
+++ estruturas_condicao1.py
@@ -0,0 +1,12 @@
+# Atribuindo o valor 29 à variável temperatura
+temperatura = 31
+
+# Definindo a função com um nome diferente
+def verificar_temperatura(temp):
+    if temp < 30:
+        print("A temperatura hoje está amena")
+    else:
+        print("Hoje está fazendo calor")
+
+# Chamando a função com a variável
+verificar_temperatura(temperatura)
